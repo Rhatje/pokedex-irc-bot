8a90761ddc13fd86b2f257aed67483a04575790f
jerodev <jeroen@deviaene.eu>
2016-07-08 14:23:36 +0200

    Build - Uglify javascript code

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var uglify = require("gulp-uglify");
 
 gulp.task("default", function () {
   return gulp.src("src/**/*.js")
     .pipe(babel())
+    .pipe(uglify())
     .pipe(gulp.dest("dist"));
 });
 
